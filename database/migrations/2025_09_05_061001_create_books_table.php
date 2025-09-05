cacd62f2463004b3c8e8cd75805d4f4b0c8bec4e
imanolcier21 <92285440+imanolcier21@users.noreply.github.com>
2025-09-05 00:27:46 -0600

    se modificaron las migraciones de books y tasks para agregar los campos correspondientes a cada tabla

--- database/migrations/2025_09_05_061001_create_books_table.php
+++ database/migrations/2025_09_05_061001_create_books_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('books', function (Blueprint $table) {
-            $table->id();
+            $table->id('BookID');
+            $table->string('Title');
+            $table->foreignId('UserId')->references('UserID')->on('users');
             $table->timestamps();
         });
     }
