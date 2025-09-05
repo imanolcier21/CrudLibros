68d7ed2d51baa6337401fdc945a3d767de171310
imanolcier21 <92285440+imanolcier21@users.noreply.github.com>
2025-09-05 02:29:51 -0600

    Se puso la primarykey para la tabla user esto para que no cause conflicto con brezze

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,8 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
+
+    protected $primaryKey = "UserID";
     protected $fillable = [
         'name',
         'email',
