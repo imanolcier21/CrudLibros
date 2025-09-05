1ceeb99498c675c086d9319e042bf2c0e4f74d0e
imanolcier21 <92285440+imanolcier21@users.noreply.github.com>
2025-09-05 02:33:19 -0600

    Se agregaron las rutas a utilizar para books y tasks

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Livewire\Books;
+use App\Livewire\Tasks;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -11,5 +13,8 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+route::get('/books', Books::class)->name('books.index');
+route::get('/tasks', Tasks::class)->name('tasks.index');
+
 
 require __DIR__.'/auth.php';
